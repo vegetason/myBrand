a3e022cbb2716c7c8fb125001b9ab6712a5b61d6
vegetason <irakozepirlo@gmail.com>
2024-03-14 15:03:29 +0200

    Add a dashboard page

--- dashboardPage/dashboard.js
+++ dashboardPage/dashboard.js
@@ -1,34 +1,47 @@
-let articlebtn=document.querySelector('#button1')
-let modal=document.querySelector('dialog')
-let closeModal=document.querySelector('#close')
-articlebtn.addEventListener('click',()=>{
-    modal.showModal()
-    document.getElementById('description').value='';
-})
-function createArticle(description){
-    this.description=description;
-    // this.image=image;
-    return{description}
-}
-let articles=[];
-closeModal.addEventListener('click',()=>{
-    articles.push(createArticle(document.getElementById('description').value));
-    console.log(articles)
-    modal.close();
-})
-articles.forEach(article=>{
-    let div=document.createElement('div');
-    let thebutton=document.createElement('button')
-    let titleOfArticle=document.createElement('h2');
-    div.appendChild(titleOfArticle);
-    let theDialog=document.createElement('dialog');
-    let textArea=
-    theDialog.appendChild(titleOfArticle);
-    div.appendChild(theDialog);
-    div.appendChild(thebutton);
-    thebutton.textContent='Back';
-    thebutton.addEventListener('click',()=>{
-        theDialog.close();
-    })
+// let articlebtn=document.querySelector('#button1')
+// let modal=document.querySelector('dialog')
+// let closeModal=document.querySelector('#close')
+// articlebtn.addEventListener('click',()=>{
+//     modal.showModal()
+//     document.getElementById('description').value='';
+// })
+// function createArticle(description){
+//     this.description=description;
+//     // this.image=image;
+//     return{description}
+// }
+// let articles=[];
+// closeModal.addEventListener('click',()=>{
+//     articles.push(createArticle(document.getElementById('description').value));
+//     console.log(articles)
+//     modal.close();
+// })
+// articles.forEach(article=>{
+//     let div=document.createElement('div');
+//     let thebutton=document.createElement('button')
+//     let titleOfArticle=document.createElement('h2');
+//     div.appendChild(titleOfArticle);
+//     let theDialog=document.createElement('dialog');
+//     let textArea=
+//     theDialog.appendChild(titleOfArticle);
+//     div.appendChild(theDialog);
+//     div.appendChild(thebutton);
+//     thebutton.textContent='Back';
+//     thebutton.addEventListener('click',()=>{
+//         theDialog.close();
+//     })
 
+// })
+let newContent=document.querySelector('#new');
+let uploadContent=document.querySelector('#upload');
+let shareContent=document.querySelector('#share');
+let manageBlog=document.querySelector('#newContentToAdd');
+let nameOfWhatToManage=document.querySelector('#nameOfWhatToManage');
+let back1=document.querySelector('#back1');
+newContent.addEventListener('click',()=>{
+    manageBlog.showModal();
 })
+back1.addEventListener('click',()=>{
+    manageBlog.close();
+})
+
--- loginPage/login.js
+++ loginPage/login.js
@@ -1,2 +1,8 @@
 let userName=document.querySelector('#username');
-let Password=document.querySelector('#Password')
+let Password=document.querySelector('#Password');
+let form=document.querySelector('form');
+form.addEventListener('click',(e)=>{
+    if(userName.value==='irakozepirlo@gmail.com' && Password.value==='Hardock76()'){
+        alert('login successful')
+    }
+})
